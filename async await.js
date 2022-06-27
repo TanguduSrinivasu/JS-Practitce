b19ddbf592846078b92572f2647c625ccb326e74
Tangudu Srinivasu <tangudusrinivasu17@gmail.com>
2022-06-27 19:56:53 +0530

    Formatted the Code

--- async await.js
+++ async await.js
@@ -1,28 +1,27 @@
-function greetHello()
-{
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve('Hello'),2000})
+function greetHello() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello"), 2000;
     });
+  });
 }
 
-function greetWorld()
-{
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve('World'),1000})
+function greetWorld() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("World"), 1000;
     });
+  });
 }
 
-async function sequentialStart()
-{
-    const hello = await greetHello();
-    console.log(hello);
+async function sequentialStart() {
+  const hello = await greetHello();
+  console.log(hello);
 
-    const world = await greetWorld();
-    console.log(world);
+  const world = await greetWorld();
+  console.log(world);
 }
 
-sequentialStart()  
+sequentialStart();
 
 //output can be viewed in console of any browser
